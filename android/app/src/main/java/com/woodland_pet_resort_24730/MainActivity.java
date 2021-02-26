626b32d05e530a7dd9ebb854d721d29b0ccead28
Crowdbotics <team@crowdbotics.com>
2021-02-26 19:53:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/woodland_pet_resort_24730/MainActivity.java
@@ -0,0 +1,15 @@
+package com.woodland_pet_resort_24730;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "woodland_pet_resort_24730";
+    }
+}
---
+++ ios/woodland_pet_resort_24730Tests/woodland_pet_resort_24730Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface woodland_pet_resort_24730 : XCTestCase
+
+@end
+
+@implementation woodland_pet_resort_24730
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
